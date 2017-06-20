633f5a1062d6ef9ab989696417333d53ead9e710
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-20 17:07:48 -0500

    adding more functions

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,26 @@ var playerOneSymbol = 'X';
 var playerTwoSymbol = 'O';
 var currentTurn = playerOneSymbol;
 var board = document.querySelector('.board');
+var clicked = 0;
 
-board.addEventListener('click', function (e) {
+
+function checkForTurn(e) {
   e.target.innerHTML = currentTurn;
   currentTurn = currentTurn === playerOneSymbol ? playerTwoSymbol : playerOneSymbol;
-});
+  clicked++;
+  console.log(clicked);
+
+  if (clicked === 9 ) {
+    checkForWinner();
+  }
+}
+
+function checkForWinner () {
+  var squares = Array.prototype.slice.call(document.querySelectorAll('.square'), 0);
+  var symbols = squares.map(function (square) {
+    return square.innerHTML;
+  });
+  console.log(symbols);
+}
+
+board.addEventListener('click', checkForTurn);
