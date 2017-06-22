50ad782edb5163dd142a035d891f4b81d4ac5d19
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-22 17:47:50 -0500

    completed winner logic

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,28 @@
 
 import { forEach, winningCombos } from './utils';
 
+Array.prototype.compare = function(testArr) {
+  if (this.length != testArr.length) return false;
+  for (var i = 0; i < testArr.length; i++) {
+    if (this[i].compare) {
+      if (!this[i].compare(testArr[i])) return false;
+    }
+    else if (this[i] !== testArr[i]) return false;
+  }
+  return true;
+}
+
+
 const game = {
   cells: document.querySelectorAll('.cell'),
   players: [
     {
+      name: 'Player One',
       value: 'X',
       selection: []
     },
     {
+      name: 'Player Two',
       value: 'O',
       selection: []
     }
@@ -31,15 +45,19 @@ forEach(game.cells, (index, value) => {
   value.addEventListener('click', (e) => {
     const player1 = game.players[0];
     const player2 = game.players[1];
+    const selection = game.turn.selection;
     const data = e.target.dataset;
 
     if (data['clicked'] === 'false') {
       data['clicked'] = 'true';
       game.board.splice(index, 1, game.turn.value);
       e.target.innerHTML = game.board[index];
+      selection.push(index);
+      selection.sort((a, b) => a - b);
+      if (selection.compare([0, 3, 6]) === true) {
+        alert(`${game.turn.name} Wins!`);
+      };
       game.turn = game.turn === player1 ? player2 : player1;
-      game.turn.selection.push(index);
-      console.log(game.turn.selection);
     }
   });
 });
