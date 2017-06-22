ec3f16dd37fc5305bc34d7700be35fa31b57d535
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-22 00:17:39 -0500

    set up plan for tomorrow

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ const cellNumbers = 9;
 const state = {
   currentTurn: playerOne,
   board: []
-}
+};
 
 for (let i = 0; i < cellNumbers; i++) {
   state.board.push({
@@ -23,27 +23,30 @@ const forEach = (array, callback, scope) => {
 };
 
 const cells = document.querySelectorAll('.cell');
+
 forEach(cells, (index, value) => {
   value.addEventListener('click', (e) => {
     state.board.splice(index, 1, {
-      'value': state.currentTurn,
-      'clicked': true
+      'value': state.currentTurn
     });
     if (e.target.dataset['clicked'] === 'false') {
       e.target.innerHTML = state.board[index].value;
       state.currentTurn = state.currentTurn === playerOne ? playerTwo : playerOne;
+      console.log(state.board);
     }
     e.target.dataset['clicked'] = 'true';
   });
 });
-//
+
+// 1. Reset game
+// 2. Check for winner and report to view
+
 // function checkForWinner () {
-//   const cells = state.board;
-//
 //   const winningCombos = [
 //     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
 //   ];
 //   return winningCombos.find(function (combo) {
+//     const cells = state.board;
 //     if (cells[combo[0]] === cells[combo[1]] && cells[combo[1]] === cells[combo[2]]) {
 //       return cells[combo[0]];
 //       console.log(cells);
@@ -52,3 +55,4 @@ forEach(cells, (index, value) => {
 //     }
 //   });
 // }
+// checkForWinner();
