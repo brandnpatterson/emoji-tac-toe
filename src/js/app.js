d189c73307293ff034c29946c1ea219d7a372ea0
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-22 22:47:36 -0500

    need to solve winner

--- src/js/app.js
+++ src/js/app.js
@@ -1,65 +1,64 @@
 /**
- * Tic Tac Toe
+ * Tic Tac Toe Game
 **/
-
+// import { arraysEqual, forEach, winningCombos } from './utils';
 import { forEach, winningCombos } from './utils';
-
-Array.prototype.compare = function(testArr) {
-  if (this.length != testArr.length) return false;
-  for (var i = 0; i < testArr.length; i++) {
-    if (this[i].compare) {
-      if (!this[i].compare(testArr[i])) return false;
-    }
-    else if (this[i] !== testArr[i]) return false;
-  }
-  return true;
-}
-
 
 const game = {
   cells: document.querySelectorAll('.cell'),
-  players: [
-    {
-      value: 'X',
-      selection: []
-    },
-    {
-      value: 'O',
-      selection: []
-    }
-  ],
   start () {
+    this.players = [
+      {
+        value: 'X',
+        selection: [],
+        secondArray: []
+      },
+      {
+        value: 'O',
+        selection: [],
+        secondArray: []
+      }
+    ];
     this.board = [];
     this.turn = this.players[0];
-    forEach(game.cells, (index, value) => {
+    // custom forEach used for elements selected with querySelectorAll
+    forEach(this.cells, (index, value) => {
       this.board.push('');
-      value.innerHTML = game.board[index];
+      value.innerHTML = this.board[index];
       value.dataset['clicked'] = 'false';
     });
   }
 };
 
-forEach(game.cells, (index, value) => {
+forEach (game.cells, (index, value) => {
   value.addEventListener('click', (e) => {
-    const player1 = game.players[0];
-    const player2 = game.players[1];
+    const { board, players } = game;
+    const player1 = players[0];
+    const player2 = players[1];
     const selection = game.turn.selection;
+    const secondArray = game.turn.secondArray;
     const data = e.target.dataset;
 
     if (data['clicked'] === 'false') {
       data['clicked'] = 'true';
-      game.board.splice(index, 1, game.turn.value);
-      e.target.innerHTML = game.board[index];
+      board.splice(index, 1, game.turn.value);
+      e.target.innerHTML = board[index];
+      game.turn = game.turn === player2 ? player1 : player2;
       selection.push(index);
       selection.sort((a, b) => a - b);
-      if (selection.compare([0, 3, 6]) === true) {
-        alert(`${game.turn.value} Wins!`);
-        game.start();
-      };
-      game.turn = game.turn === player1 ? player2 : player1;
+
+      // winningCombos.map((combo) => {
+
+      // if selection includes any combos
+      // end game declare winner
+
+      //   if (contains(combo, selection) === true) {
+      //     alert(`${game.turn.value} Wins!`);
+      //     game.start();
+      //   }
+      //   game.turn = game.turn === player2 ? player1 : player2;
+      // });
     }
   });
 });
 game.start();
-
-document.querySelector('.reset').addEventListener('click', () => game.start());
