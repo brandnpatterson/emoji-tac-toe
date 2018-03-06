6e7bb4cb9ecdfc0c1bccf5dc4ce79331d00eed65
Brandon Patterson <brandnpatterson@gmail.com>
2018-03-05 20:33:32 -0600

    es6 modules

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,7 +2,7 @@
  * General Utils
 **/
 
-const utils = module.exports = {
+const utils = {
   forEach (array, callback, scope) {
     for (let i = 0; i < array.length; i++) {
       callback.call(scope, i, array[i]);
@@ -17,4 +17,6 @@ const utils = module.exports = {
   winningCombos: [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
   ]
-}
+};
+
+export default utils;
